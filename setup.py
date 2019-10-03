32483e4acfb1a07c05b7cdb43e791a428fdd51f8
neelu0 <neelu0@protonmail.com>
2019-10-03 20:43:45 +0500

    first release, added setup.py, readme and license to be added

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+   name='passman',
+   version='0.1',
+   description='simple commandline password manager that uses commandline arguments to do what its meant to do, manage passwords',
+   author='Neelu',
+   author_email='neelu0@protonmail.com',
+   packages=['passman'],  #same as name
+   install_requires=['cryptography>=2.7', 'pyperclip>=1.7.0', 'tabulate>=0.8.5', 'pyYAML>=5.1.1'],  #external packages as dependencies
+   entry_points={
+        "console_scripts": [
+            "passman=passman.passman:main",
+        ]
+    }
+)
